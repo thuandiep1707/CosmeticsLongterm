28dcacef557d25464b36971dd88a2e66f670298c
Ngọ Văn Long <teranova1707+github@gmail.com>
2023-07-18 17:05:48 +0700

    continue
    thay đổi config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-// import path from "path"
+import path from "path"
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // resolve: {
-  //   alias: {
-  //     "@": Path2D.resolve(_dirname,"./src/"),
-  //     component: `${path.}`
-  //   }
-  // }
+  resolve: {
+    alias: {
+  "compilerOptions": {
+    "baseUrl": ".",
+    "paths": {
+      "@/*": ["src/*"]
+    }
+  }
+}
+  }
 })
