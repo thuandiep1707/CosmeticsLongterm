f3c165221af7cb59e581a5b299cda9b1077f5319
Ngọ Văn Long <teranova1707+github@gmail.com>
2023-10-10 02:50:06 +0700

    Update Slides.jsx

--- src/layout/home/components/Slides.jsx
+++ src/layout/home/components/Slides.jsx
@@ -1,18 +1,28 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import {Slide1,Slide2} from './SlideChild'
 import '../style/slides.scss'
 
 const Slides = ({slideShow})=>{
-    const [numSlide, setNumSlide] = useState(3)
+    const [numSlide, setNumSlide] = useState(4)
     const [changeSlide, setChangeSlide] = useState(true)
     const handleChangeSlide = (value) => {
         let num = numSlide + value
         setChangeSlide(!changeSlide)
         if (num < 0) return setNumSlide(4)
         if (num > 4) return setNumSlide(0)
+        console.log(numSlide)
+        console.log("value", value)
         return setNumSlide(num)
     }
+    // console.log(numSlide)
+    useEffect(()=>{
+        const interval = setInterval(() => {
+            handleChangeSlide(1)
+            // console.log(1)
+        }, 3000);
+        return () => clearInterval(interval);
+    }, [changeSlide])
     return (
         <>
             {changeSlide === true ? <Slide1 slideData={slideShow[numSlide]}  handleChangeSlide={handleChangeSlide}/> : <Slide2 slideData={slideShow[numSlide]}  handleChangeSlide={handleChangeSlide}/>}
