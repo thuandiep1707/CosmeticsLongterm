bd589c6efa1ac8142ca16ea37a23559b637ecdb9
Ngọ Văn Long <75620455+Thuandiepbt1707@users.noreply.github.com>
2023-12-12 13:26:25 +0700

    Update Introduce.jsx

--- src/layout/introduce/Introduce.jsx
+++ src/layout/introduce/Introduce.jsx
@@ -2,8 +2,8 @@ import './introduce.scss'
 
 const Introduce = () => {
     return (
-        <div className="introduce">
-            introduce nè
+        <div className="blog">
+            Các bài viết đang trong giai đoạn hoàn thiện, mời quý khách quay lại sau
         </div>
     )
 }
